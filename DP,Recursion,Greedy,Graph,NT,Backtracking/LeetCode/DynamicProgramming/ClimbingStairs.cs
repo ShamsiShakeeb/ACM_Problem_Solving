59a48cb9816d4b4bf73de4d0b11bb7e81a07017a
Washiq Shamsi <jakas1gy@gmail.com>
2023-06-30 12:43:06 +0600

    LeetCode & DPRGGNTB

---
+++ DP,Recursion,Greedy,Graph,NT,Backtracking/LeetCode/DynamicProgramming/ClimbingStairs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingJourney.LeetCode.DynamicProgramming
+{
+    public class ClimbingStairs
+    {
+        Dictionary<int, int> memo = new Dictionary<int, int>();
+        public int ClimbStairs(int n)
+        {
+            if (n == 0)
+                return 1;
+            else if (n < 0)
+                return 0;
+            int ival;
+            var IsExist = memo.TryGetValue(n, out ival);
+            if (IsExist)
+                return ival;
+            memo[n] = ClimbStairs(n - 1) + ClimbStairs(n - 2);
+            return memo[n];
+        }
+    }
+}
